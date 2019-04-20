df1219dfad3ac3fc5ff5d8a69de60ba4b92319b1
xiaoran <ran.xiao@tendcloud.com>
2019-04-20 13:18:44 +0800

    init

---
+++ src/main/java/com/lucky/SecondsKillWebApplication.java
@@ -0,0 +1,13 @@
+package com.lucky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondsKillWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondsKillWebApplication.class, args);
+    }
+
+}
