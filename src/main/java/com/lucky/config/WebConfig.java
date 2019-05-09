3ba1fe33c455936e341c5dcf1fd565d2cc685a07
xiaoran <xiaoran737@163.com>
2019-05-09 19:44:40 +0800

    解决spring boot静态资源不能访问的问题

--- src/main/java/com/lucky/config/WebConfig.java
+++ src/main/java/com/lucky/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.lucky.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 import java.util.List;
@@ -13,6 +14,15 @@ public class WebConfig  extends WebMvcConfigurationSupport {
 	@Autowired
 	UserArgumentResolver userArgumentResolver;
 
+	/**
+	 * 静态资源访问路径指明
+	 * @param registry
+	 */
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+	}
+
     /**
      * 参数先校验，再将参数赋值给controller
      * @param argumentResolvers
@@ -21,6 +31,6 @@ public class WebConfig  extends WebMvcConfigurationSupport {
 	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
 		argumentResolvers.add(userArgumentResolver);
 	}
-	
+
 	
 }
